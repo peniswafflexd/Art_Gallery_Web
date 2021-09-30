bd698415c858db2613a2e09432e410ef0bb4eca3
Devon Peniswaffle <tinnyfacexd@gmail.com>
2021-09-30 18:37:46 +1300

    added post and delete calls for art/:artwork_id routes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import {add_user, get_all_art, get_art_by_id, user_login} from "./controller";
+import {add_art, add_user, get_all_art, remove_artwork, update_artwork, user_login} from "./controller";
 
 const express = require('express');
 const router = express.Router();
@@ -11,7 +11,7 @@ const app = express();
 const ONE_DAY = 1000 * 60 * 60 * 24;
 
 //session middleware
-let session;
+// let session;
 app.use(sessions({
     secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
     saveUninitialized: true,
@@ -90,13 +90,37 @@ router.get('/order', (req, res) => {
     res.render('pages/order');
 });
 
+router.post("/art", (req, res) => {
+    let author = req.body.author
+    let description = req.body.desc
+    let media_url = req.body.media
+    let price = req.body.price
+    add_art(author, description, media_url, price)
+        .then(data => {
+            populateArtworkMap(data);
+        }).catch(err => {
+            console.error(err)
+    })
+});
+
+router.delete("/art/:artwork_id", (req, res) => {
+    let artwork_id = req.params.artwork_id
+    remove_artwork(artwork_id)
+        .then(() => {
+            get_all_art().then(data => populateArtworkMap(data));
+        }).catch(err => {
+            if(err === "artwork_in_donations") res.send("Cannot delete this artwork")
+            else if (err === "artwork_in_orders") res.send("Cannot delete this artwork")
+            else console.error(err);
+    })
+})
+
 /**
  * Returns page of the artwork from the given artwork ID in the
  * URL
  */
 router.get("/art/:artwork_id", (req, res) => {
     let artwork_id = req.params.artwork_id
-    console.log(artwork_id)
     let currentArtwork = artworkMap.get(artwork_id);
     if(currentArtwork){
         res.render("pages/artwork", {currentArtwork});
@@ -105,14 +129,32 @@ router.get("/art/:artwork_id", (req, res) => {
     }
 })
 
+app.put("/art/:artwork_id", (req, res) => {
+    let artwork_id = req.params.artwork_id
+    let price = req.body.price
+    let author = req.body.author
+    let media_url = req.body.media
+    let description = req.body.desc
+    let update = {}
+    if(price) update.price = price
+    if(author) update.author = author
+    if(media_url) update.media_url = media_url
+    if(description) update.description = description
+    console.log(update);
+    update_artwork(artwork_id, update).then(data => {
+        populateArtworkMap(data);
+    }).catch(err => {
+        console.error(err);
+    })
+})
+
 router.post("/login", (req, res) => {
     let username = req.body.user
     let password = req.body.pass
     user_login(username, password)
         .then(data => {
-            session = req.session;
-            session.userid = username;
-            session.user = data
+            req.session.userid = username;
+            req.session.user = data
             console.log(`${username} logged in!`);
             res.redirect("/");
         })
@@ -129,9 +171,8 @@ router.post('/sign-up', (req, res) => {
     let firstname = req.body.first
     let lastname = req.body.last
     add_user(username, password, firstname, lastname, false).then((data) => {
-        session = req.session;
-        session.userid = username;
-        session.user = data
+        req.session.userid = username;
+        req.session.user = data
         res.redirect("/");
         console.log("New user created: " + username);
     }).catch(err => {
